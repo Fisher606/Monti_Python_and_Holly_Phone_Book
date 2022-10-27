9b1225f1bacc40542c9c1136b0332aa26c2db17f
Vadim <fishermanvadik@gmail.com>
2022-10-28 02:02:06 +0300

    Настроил модуля2

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
-from os.path  
+
+from genericpath import exists
+from pathlib import Path
 from Create_CSV import creating
 from Write_File import writing_scv
 from Write_File import writing_txt
 
 
-path = 'Phonebook.csv'
-valid = exists(path)
+Path = 'Phonebook.csv'
+valid = exists(Path)
 if not valid:
     creating()
 
